f4eae9842cba303871e96420a5bd226588c60a0a
Jay McCoy <jaymccoy300@gmail.com>
2017-10-28 21:44:14 -0700

    User mailer fix

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,2 +1,10 @@
 class UserMailer < ApplicationMailer
+  def send_simple_message
+    RestClient.post "https://api:key-f3d19b57ef46f7e0ac7847309b062c8f"
+        "@api.mailgun.net/v3/sandbox76bfa7180ef34b0ba87d9a8eabb063b1.mailgun.org/messages",
+        :from => "Mailgun Sandbox <postmaster@sandbox76bfa7180ef34b0ba87d9a8eabb063b1.mailgun.org>",
+        :to => "Heroku <app79927891@heroku.com>",
+        :subject => "Hello Heroku",
+        :text => "Congratulations Heroku, you just sent an email with Mailgun!  You are truly awesome!"
+  end
 end
